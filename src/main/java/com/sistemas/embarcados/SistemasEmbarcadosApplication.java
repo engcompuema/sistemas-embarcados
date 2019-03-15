0dbcf33b045292a0994c7f351f17802bc48d091c
Lucas <lucas.ferreira@seati.ma.gov.br>
2019-03-15 12:04:19 -0300

    Commit Inicial

---
+++ src/main/java/com/sistemas/embarcados/SistemasEmbarcadosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas.embarcados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasEmbarcadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasEmbarcadosApplication.class, args);
+	}
+
+}
